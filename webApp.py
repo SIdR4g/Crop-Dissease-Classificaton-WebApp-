28367ef6741d87cf66638b8db1c836e69656a215
Siddyant Das <78850085+SIdR4g@users.noreply.github.com>
2022-07-20 10:06:52 +0530

    Update webApp.py

--- webApp.py
+++ webApp.py
@@ -1,31 +1,31 @@
 import streamlit as st
 import streamlit.components.v1 as stc
 
-# File Processing Pkgs
-import pandas as pd
-import docx2txt
+import numpy as np
+import seaborn as sb
+import matplotlib.pyplot as plt
+import matplotlib.style as style
+import time
 from PIL import Image 
-from PyPDF2 import PdfFileReader
-import pdfplumber
 from keras.models import load_model
 import tensorflow as tf
 import numpy as np
 import pickle
-import h5py as h5
 
 tfds = tf.data.Dataset
 
-
+import matplotlib.style as style
+style.use('seaborn-darkgrid')
 
 @st.cache
 def load_image(image_file):
 	img = Image.open(image_file)
 	return img 
 
-
+plt.rcParams['font.family'] = "serif"
 
 def main():
-    st.title("Crop dissease classification")
+    st.title("      Crop dissease classification")
     st.sidebar.header("Crop Species")
 
     menu = ["Home","Apple", "Cherry", "Corn", "Grape", "Peach", "Potato", "Strawberry"]
@@ -36,194 +36,55 @@ def main():
 
         st.subheader("Choose the Crop for which you want to identify the dissease")
 
-    
-    if choice == "Apple":
-
-        st.subheader(choice)
-        image_file = st.file_uploader("Upload Image",type=['png','jpeg','jpg'])
-        if image_file is not None:
-
-            st.write(type(image_file))
-
-            img = load_image(image_file)
-            st.image(img,width=480)
-            st.write(image_file)
-
-            IMG_SIZE = (1,128, 128,3)
-            img_array = np.resize(np.array(img),IMG_SIZE)
-            st.write(img_array.shape)
-
-            dataset = tf.data.Dataset.from_tensor_slices(img_array)
-            dataset = dataset.batch(1)
-            with open(choice+'_class.pkl','rb') as clas:
-                classes = pickle.load(clas)
-
-
-            reconstructed_model = load_model(choice+".h5")
-            out = reconstructed_model.predict(dataset)
-            st.write(classes[np.argmax(out)])
-
-    elif choice == "Cherry":
-
-        st.subheader(choice)
-        image_file = st.file_uploader("Upload Image",type=['png','jpeg','jpg'])
-        if image_file is not None:
-
-            st.write(type(image_file))
-
-            img = load_image(image_file)
-            st.image(img,width=480)
-            st.write(image_file)
-
-            IMG_SIZE = (1,128, 128,3)
-            img_array = np.resize(np.array(img),IMG_SIZE)
-            st.write(img_array.shape)
-
-            dataset = tf.data.Dataset.from_tensor_slices(img_array)
-            dataset = dataset.batch(1)
-
-            with open(choice+'_class.pkl','rb') as clas:
-                classes = pickle.load(clas)
-
-            reconstructed_model = load_model(choice+".h5")
-            out = reconstructed_model.predict(dataset)
-            st.write(classes[np.argmax(out)])
-
-    elif choice == "Peach":
-
-        st.subheader(choice)
-        image_file = st.file_uploader("Upload Image",type=['png','jpeg','jpg'])
-        if image_file is not None:
-
-            st.write(type(image_file))
-            img = load_image(image_file)
-            st.image(img,width=480)
-            st.write(image_file)
-
-            IMG_SIZE = (1,128, 128,3)
-            img_array = np.resize(np.array(img),IMG_SIZE)
-            st.write(img_array.shape)
-            dataset = tf.data.Dataset.from_tensor_slices(img_array)
-            dataset = dataset.batch(1)
-
-
-            with open(choice+'_class.pkl','rb') as clas:
-                classes = pickle.load(clas)
-
-
-            reconstructed_model = load_model(choice+".h5")
-            out = reconstructed_model.predict(dataset)
-            st.write(classes[np.argmax(out)])
-
-    elif choice == "Potato":
-
-        st.subheader(choice)
-        image_file = st.file_uploader("Upload Image",type=['png','jpeg','jpg'])
-        if image_file is not None:
-
-            st.write(type(image_file))
-
-            img = load_image(image_file)
-            st.image(img,width=480)
-            st.write(image_file)
-
-            IMG_SIZE = (1,128, 128,3)
-            img_array = np.resize(np.array(img),IMG_SIZE)
-            st.write(img_array.shape)
-
-            dataset = tf.data.Dataset.from_tensor_slices(img_array)
-            dataset = dataset.batch(1)
-            with open(choice+'_class.pkl','rb') as clas:
-                classes = pickle.load(clas)
-
-
-            reconstructed_model = load_model(choice+".h5")
-            out = reconstructed_model.predict(dataset)
-            st.write(classes[np.argmax(out)])
-
-
-    elif choice == "Corn":
-
-        st.subheader(choice)
-        image_file = st.file_uploader("Upload Image",type=['png','jpeg','jpg'])
-        if image_file is not None:
-
-            st.write(type(image_file))
-            img = load_image(image_file)
-            st.image(img,width=480)
-            st.write(image_file)
-
-            IMG_SIZE = (1,128, 128,3)
-            img_array = np.resize(np.array(img),IMG_SIZE)
-            st.write(img_array.shape)
-
-            dataset = tf.data.Dataset.from_tensor_slices(img_array)
-            dataset = dataset.batch(1)
-            with open('_class.pkl','rb') as clas:
-                classes = pickle.load(clas)
-
-
-            reconstructed_model = load_model(choice+".h5")
-            out = reconstructed_model.predict(dataset)
-            st.write(classes[np.argmax(out)])
-
-
-    elif choice == "Grape":
-
-        st.subheader(choice)
-        image_file = st.file_uploader("Upload Image",type=['png','jpeg','jpg'])
-        if image_file is not None:
-
-            st.write(type(image_file))
-            img = load_image(image_file)
-            st.image(img,width=480)
-            st.write(image_file)
-
-            IMG_SIZE = (1,128, 128,3)
-            img_array = np.resize(np.array(img),IMG_SIZE)
-            st.write(img_array.shape)
-
-            dataset = tf.data.Dataset.from_tensor_slices(img_array)
-            dataset = dataset.batch(1)
-            with open(choice+'_class.pkl','rb') as clas:
-                classes = pickle.load(clas)
-
-
-            reconstructed_model = load_model(choice+".h5")
-            out = reconstructed_model.predict(dataset)
-            st.write(classes[np.argmax(out)])
-
-
-    elif choice == "Strawberry":
-
-        st.subheader(choice)
-        image_file = st.file_uploader("Upload Image",type=['png','jpeg','jpg'])
-        if image_file is not None:
-
-            st.write(type(image_file))
-            img = load_image(image_file)
-            st.image(img,width=480)
-            st.write(image_file)
-
-            IMG_SIZE = (1,128, 128,3)
-            img_array = np.resize(np.array(img),IMG_SIZE)
-            st.write(img_array.shape)
-            dataset = tf.data.Dataset.from_tensor_slices(img_array)
-            dataset = dataset.batch(1)
-            with open(choice+'_class.pkl','rb') as clas:
-                classes = pickle.load(clas)
-
-
-            reconstructed_model = load_model(choice+".h5")
-            out = reconstructed_model.predict(dataset)
-            st.write(classes[np.argmax(out)])
 
 
     
+    elif choice in menu[1:]:
+
+        st.subheader(choice)
+        image_file = st.file_uploader("Upload Image",type=['png','jpeg','jpg'])
+        if image_file is not None:
+
+            # To See Details
+            st.write(type(image_file))
+            # st.write(dir(image_file))
+            # file_details = {"Filename":image_file.name,"FileType":image_file.type,"FileSize":image_file.size}
+            # st.write(file_details)
+
+            img = load_image(image_file)
+            st.image(img,width=480)
+            st.write(image_file)
+
+            IMG_SIZE = (1,128, 128,3)
+            img_array = np.resize(np.array(img),IMG_SIZE)
+            st.write(img_array.shape)
+            # img_array = tf.image.resize(img_array, size=IMG_SIZE)
+            # st.write(img_array)
+
+            dataset = tf.data.Dataset.from_tensor_slices(img_array)
+            # dataset = dataset.map(_parse_function)
+            dataset = dataset.batch(1)
+            with open(choice+'_class.pkl','rb') as clas:
+                classes = pickle.load(clas)
+
+            with st.spinner("Loading....."):
+                time.sleep(1)
+            reconstructed_model = load_model(choice+".h5")
+            out = reconstructed_model.predict(dataset)
+            
+            fig = plt.figure(figsize=(10, 4))  
+            sb.set_context('poster')          
+            b = plt.barh( y=classes, width = np.reshape(out, (-1,)), color = 'darkred')
+            b[np.argmax(out)].set_color('green')
+            plt.xlim(0,1)
+            plt.xlabel("Probability ofeach dissease")            
+            
+            st.pyplot(fig)
+            # st.write(classes[np.argmax(out)])
+
 
     else:
         st.subheader("About")
-        st.info("Built with Streamlit")
         st.info("by S1DR4G")
         st.text("Siddyant Das")
 
